06bc44b1ae48f74321351f1e187c1d46384d9280
penline01 <penline01@naver.com>
2022-04-18 11:37:13 +0900

    webpage

---
+++ djangoWebpage/djangoWebpage/urls.py
@@ -0,0 +1,23 @@
+"""djangoWebpage URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+# 서버의 루트주소/디렉토리/ 라는 주소에 앱 url을 연결시킨다는 의미.
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('main/', include('mainpage.urls'))
+]
